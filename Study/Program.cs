9bcd1c024ce2ebdc3058fa984f1865bff7af9c6e
Надя <nadya.la2016@yandex.ru>
2021-08-11 19:17:17 +0300

    провека внесенных данных прогноз, создала user и admin.

--- Study/Program.cs
+++ Study/Program.cs
@@ -14,6 +14,13 @@ namespace Study
             }
             for (int i = 0; i < 10; i++)
                 arrayForecast[i].GetForecast();
+            
+
+            Admin adminBob = new Admin("Bob", 23);
+            User en = new User("En", 18);
+            en.PrintUs(arrayForecast[9]);
+            adminBob.SetHumidityAd(60, arrayForecast[9]);
+            adminBob.PrintAd(arrayForecast[9]);
             Console.ReadLine();
         } 
         static Forecast CreatingAForecast(int n)
@@ -36,7 +43,7 @@ namespace Study
     {
         Sunny,
         Cloudy,
-        LightRain, //shower
+        LightRain,
         HeavyRain,
         Thunderstorm
     }
@@ -55,29 +62,65 @@ namespace Study
     class Forecast
     {
         private DateTime date;
-        private int temperatureMax;
+        private int temperatureMax = 20;
         private int temperatureMin;
-        private int pressure;
-        private int humidity;
-        private int windStrength;
-        private EDirection windDirection;
+        private int pressure = 740;
+        private int humidity = 50;
+        private int windStrength = 2;
+        private EDirection windDirection = EDirection.North;
         private EVariantsPrecipitation precipitation;
 
         public Forecast(DateTime date, int temperatureMax, int temperatureMin, int pressure, int humidity, int windStrength, EDirection windDirection, EVariantsPrecipitation precipitation)
         {
             this.date = date;
-            this.temperatureMax = temperatureMax;
-            if (temperatureMax >= temperatureMin)
-                this.temperatureMin = temperatureMin;
-            if (pressure < 755 && pressure > 730)
-                this.pressure = pressure;
-            if (humidity > 0)
-                this.humidity = humidity;
-            if (windStrength > 0)
-                this.windStrength = windStrength;
-            this.windDirection = windDirection;
-            this.precipitation = precipitation;
+            SetTemperature(temperatureMax, temperatureMin);
+            SetPressure(pressure);
+            SetHumidity(humidity);
+            SetWind(windStrength, windDirection);
+            SetPrecipitation(precipitation);
         }
+        public void SetTemperature (int tMax, int tMin)
+        {
+            if (tMax >= -267 && tMax <= 50)
+                this.temperatureMax = tMax;
+            else
+                Console.WriteLine($"The maximum temperature value is out of range (-267) - 50C. Maximum temperature is set {temperatureMax}.");
+            if (tMin <= this.temperatureMax)
+                this.temperatureMin = tMin;
+            else
+            {
+                this.temperatureMin = temperatureMax - 5;
+                Console.WriteLine($"The minimum temperature exceeds the maximum. The minimum temperature is set {temperatureMin}.");
+            }
+
+        }
+        public void SetPressure (int setPressure)
+        {
+            if (setPressure > 735 && setPressure < 750)
+                this.pressure = setPressure;
+            else
+                Console.WriteLine($"The pressure value is out of range 735 - 750mmHg. Pressure is set = {this.pressure}mmHg.");
+        }
+        public void SetHumidity(int setHumidity)
+        {
+            if (setHumidity >= 0 && setHumidity <= 100)
+                this.humidity = setHumidity;
+            else
+                Console.WriteLine($"The humidity value is out of range 0 - 100%. Humidity is set {this.humidity}%");
+        }
+        public void SetWind(int strength, EDirection setDirection)
+        {
+            if (strength >= 0 && strength <= 67)
+                this.windStrength = strength;
+            else
+                Console.WriteLine($"The wind strength value is out of range 0 - 67 m/s. Wind strength {this.windStrength}m/s.");
+            this.windDirection = setDirection;
+        }
+        public void SetPrecipitation(EVariantsPrecipitation setPrecipitat)
+        {
+            this.precipitation = setPrecipitat;
+        }
+
 
         public void GetForecast()
         {
@@ -87,6 +130,70 @@ namespace Study
             Console.WriteLine($"Humidity: {humidity}%");
             Console.WriteLine($"Wind: {windStrength} m/s, {windDirection}");
             Console.WriteLine($"Precipitation: {precipitation}\n");
+        }
+    }
+    class Person
+    {
+        private string name;
+        private int age = 18;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            if (age >= 0)
+                this.age = age;
+            else
+                Console.WriteLine("The age cannot be less than 0. Age is set 18.");
+        }
+        public void PrintForecast (Forecast n)
+        {
+            n.GetForecast();
+        }
+    }
+
+    class User : Person
+    {
+        public User (string name, int age) : base (name, age)
+        { }
+        public void PrintUs(Forecast n)
+        {
+            Console.WriteLine("The user looks at it.");
+            base.PrintForecast(n);
+        }
+    }
+
+    class Admin : Person
+    {
+        public Admin (string name, int age) : base (name, age)
+        {
+
+        }
+        public void PrintAd(Forecast n)
+        {
+            Console.WriteLine("The admin looks at it.");
+            base.PrintForecast(n);
+        }
+
+
+        public void SetTemperatureAd(int tMaxAd, int tMinAd, Forecast forecast)
+        {
+            forecast.SetTemperature(tMaxAd, tMinAd);
+        }
+        public void SetPressureAd (int pressureAd, Forecast forecast)
+        {
+            forecast.SetPressure(pressureAd);
+        }
+        public void SetHumidityAd (int humidityAd, Forecast forecast)
+        {
+            forecast.SetHumidity(humidityAd);
+        }
+        public void SetWindAd(int strengthAd, EDirection directionAd, Forecast forecast)
+        {
+            forecast.SetWind(strengthAd, directionAd);
+        }
+        public void SetPrecipitationAd(EVariantsPrecipitation precipitationAd, Forecast forecast)
+        {
+            forecast.SetPrecipitation(precipitationAd);
         }
     }
 }
